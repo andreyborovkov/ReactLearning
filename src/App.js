21e02303a8e542611f5ee61751e2493cdd163abd
andreyborovkov <andrey_borovkov@yahoo.com>
2020-03-03 23:49:54 -0500

    setState added

--- src/App.js
+++ src/App.js
@@ -8,20 +8,27 @@ class App extends Component {
       { name: 'Max', age: 28},
       { name:"Stephanie", age: 21},
       { name:"Andrey", age: 24}
-
-    ]
+    ],
+    otherState: 'some other value'
   }
 
   switchNameHandler = () => {
-    console.log('WAs clicked!');
-  }
+    //console.log('WAs clicked!');
+    this.setState({
+      persons: [
+        { name: 'Maximilian', age: 28},
+        { name:"Stephanie", age: 21},
+        { name:"Andrey", age: 27}
+      ]
+    });
+  };
 
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <button onClgit adick={this.switchNameHandler}>Switch Name</button>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing </Person>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
